582d23e02f39543977f14bf64c85fa4e44183364
Nikunj Shah <59753817+nicks204@users.noreply.github.com>
2024-04-06 21:28:49 +0530

    Update BasePage.java

--- src/test/java/testCases/BasePage.java
+++ src/test/java/testCases/BasePage.java
@@ -2,7 +2,7 @@ package testCases;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
