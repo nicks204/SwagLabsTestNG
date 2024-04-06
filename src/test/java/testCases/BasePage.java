cca560aff09b01e93f3ef51f1a333c67555df3fb
Nikunj Shah <59753817+nicks204@users.noreply.github.com>
2024-04-06 21:29:22 +0530

    Update BasePage.java

--- src/test/java/testCases/BasePage.java
+++ src/test/java/testCases/BasePage.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BasePage {
 	WebDriver driver;
 
